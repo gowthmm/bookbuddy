469a82ab2120a9c1363258e083c74d33146064fd
gowtham <gowthambenny07@gmail.com>
2026-09-23 16:44:21 +0530

    fixer issues with gemini llm model and checked api endpoints in fastapi swagger

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ from typing import Optional
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-3.6-flash")
 
 DATABASE_URL = "sqlite:///./books.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
